2689e68bf64f99873225d6c6cc0a4431e449d49d
Andrea Pettenello <andreapettenello@gmail.com>
2023-08-28 11:24:44 +0200

    Versuni analysis script

---
+++ versuni_analysis.js
@@ -0,0 +1,21 @@
+
+
+const productIds = db.products.distinct("_id", { scopes: /^versuni_contract\./gi })
+
+const reviews = db.reviews.find({ productIds: { $in: productIds}  })
+
+const res = db.reviews.aggregate([{
+  $match: {
+    productIds: { $in: productIds}
+  }
+}, {
+  $group:{
+    _id: null,
+    numberOfDocuments: {$sum: 1},
+    numberOfWords: { $sum: "$numberOfWords" },
+    maxNumberOfWords: {$max: "$numberOfWords" },
+    avgNumberOfWords: {$avg: "$numberOfWords"  }
+  }
+}])
+
+printjson(res.toArray())
